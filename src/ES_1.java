f09a15b5cf71987c6a1fae712d28f0f1ced6c1c5
12kpro <97319981+12kpro@users.noreply.github.com>
2023-05-04 16:48:51 +0200

    d3 final

--- src/ES_1.java
+++ src/ES_1.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class ES_1 {
 
 	public static void main(String[] args) {
 		System.out.println(stringaPariDispari("ciaoz"));
 		System.out.println(annoBisestile(2020));
+		System.out.println(numeroInLettere(4));
+		countDown(3);
+		testWhile();
 	}
 
 	static String stringaPariDispari(String str) {
@@ -14,6 +18,49 @@ public class ES_1 {
 			return true;
 		}else {
 			return false;
+		}
+	}
+	static String numeroInLettere(int num) {
+		String[] intToString = new String[4];
+		intToString[0] = "zero";
+		intToString[1] = "uno";
+		intToString[2] = "due";
+		intToString[3] = "tre";
+		
+		switch(num) {
+		  case 0,1,2,3:
+			  return intToString[num];
+		  default:
+			  return "Not in range >= 0; <= 3";
+		}
+	}
+	static void testWhile() {
+		Scanner scanner = new Scanner (System.in);
+	      
+		while (true) {
+			System.out.println("Enter a string or :q to quit");
+			String str = scanner.nextLine();
+			if(str.equals(":q")) break;			
+			String[] splitString = str.split("");
+			int i = 0;
+
+			while( splitString.length > 0 && i < splitString.length)
+			{
+				
+				//i + 1 == splitString.length ? System.out.print("\n") : System.out.print(splitString[i] + ",");  
+				if (i + 1 == splitString.length) {
+					System.out.print(splitString[i] + "\n");
+				}else {
+					System.out.print(splitString[i] + ",");
+				}
+			  
+			  i++;
+			}	
+		}
+	}
+	static void countDown(int num) {
+		for (int i = num; i >= 0 ; i--) {
+			System.out.println(i);
 		}
 	}
 }
